5743bd643ec5b42ea8688802d5c49db1e69a44cc
Santeri Kottari <kosaju99@gmail.com>
2024-09-26 09:13:55 +0300

    kurssitiedot-step4 refactor to use an array

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -8,6 +8,7 @@ const Header = (props) => {
 
 const Content = (props) => {
   console.log(props)
+  console.log(props.parts[0])
   return (
     <div>
       <Part part={props.parts[0]}/>
@@ -29,33 +30,36 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  const total = props.count[0].exercises + props.count[1].exercises + props.count[2].exercises
   return (
     <div>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {total}</p>
     </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total count={parts} />
     </div>
   )
 }
