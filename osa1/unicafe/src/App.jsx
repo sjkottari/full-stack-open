fa14b30eacad0b8549154213229342ef5da38938
Santeri Kottari <kosaju99@gmail.com>
2024-10-03 14:01:27 +0300

    Implement unicafe-step1

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const incrementGood = () => setGood(good + 1)
+
+  const incrementNeutral = () => setNeutral(neutral + 1)
+
+  const incrementBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={incrementGood} text="good" />
+      <Button handleClick={incrementNeutral} text="neutral" />
+      <Button handleClick={incrementBad} text="bad" />
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  )
+}
+
+export default App
