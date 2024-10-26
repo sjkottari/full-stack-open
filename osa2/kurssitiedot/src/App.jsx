e3504972141e26717585bdf3b0815cd3918d1d1b
Santeri Kottari <kosaju99@gmail.com>
2024-10-26 21:48:21 +0300

    kurssitiedot-step6 new Course component

---
+++ osa2/kurssitiedot/src/App.jsx
@@ -0,0 +1,60 @@
+const Header = ({name}) => (
+  <div>
+    <h1>{name}</h1>
+  </div>
+)
+
+const Content = ({parts}) => (
+  <div>
+    {parts.map(part =>
+      <Part key={part.id} part={part} />
+    )}
+  </div>
+)
+
+const Part = ({part}) => (
+  <div>
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  </div>
+)
+
+const Course = ({course}) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+  </div>
+)
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
