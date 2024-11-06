35cce3cbe02b5a5604e215572256dc727f2a3347
Santeri Kottari <kosaju99@gmail.com>
2024-11-06 12:10:30 +0200

    step7 & step8 total exercises with reduce

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -20,10 +20,19 @@ const Part = ({part}) => (
   </div>
 )
 
+const Total = ({parts}) => (
+  <div>
+    <b>Total of {parts.reduce((sum, part) =>
+      sum + part.exercises, 0
+    )} exercises</b>
+  </div>
+)
+
 const Course = ({course}) => (
   <div>
     <Header name={course.name} />
     <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </div>
 )
 
