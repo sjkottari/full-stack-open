49738d318c11c016e12361b24b8533da7478c00f
Santeri Kottari <kosaju99@gmail.com>
2024-11-08 16:28:01 +0200

    kurssitiedot separate Course module

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,44 +1,4 @@
-const Header = ({name}) => (
-  <div>
-    <h2>{name}</h2>
-  </div>
-)
-
-const Content = ({parts}) => (
-  <div>
-    {parts.map(part =>
-      <Part key={part.id} part={part} />
-    )}
-  </div>
-)
-
-const Part = ({part}) => (
-  <div>
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  </div>
-)
-
-const Total = ({parts}) => (
-  <div>
-    <b>Total of {parts.reduce((sum, part) =>
-      sum + part.exercises, 0
-    )} exercises</b>
-  </div>
-)
-
-const Course = ({courses}) => (
-  <div>
-    {courses.map(course =>
-      <div key={course.id}>
-        <Header name={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-      </div>
-    )}
-  </div>
-)
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
